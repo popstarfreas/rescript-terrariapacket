1a2981d5b4e9dc25312641ffe3c821283cdf6f9b
popstarfreas <6698249+popstarfreas@users.noreply.github.com>
2023-09-03 13:28:01 +0100

    fix: Compile typescript

---
+++ src/packet/Packet_TileSquareSend.gen.js
@@ -0,0 +1,9 @@
+"use strict";
+/* TypeScript file generated from Packet_TileSquareSend.resi by genType. */
+/* eslint-disable import/first */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.toBuffer = exports.parse = void 0;
+// @ts-ignore: Implicit any on import
+const Packet_TileSquareSendBS = require('./Packet_TileSquareSend.bs');
+exports.parse = Packet_TileSquareSendBS.parse;
+exports.toBuffer = Packet_TileSquareSendBS.toBuffer;
---
+++ src/packet/Packet_WorldInfo.gen.js
@@ -0,0 +1,9 @@
+"use strict";
+/* TypeScript file generated from Packet_WorldInfo.resi by genType. */
+/* eslint-disable import/first */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.toBuffer = exports.parse = void 0;
+// @ts-ignore: Implicit any on import
+const Packet_WorldInfoBS = require('./Packet_WorldInfo.bs');
+exports.parse = Packet_WorldInfoBS.parse;
+exports.toBuffer = Packet_WorldInfoBS.toBuffer;
