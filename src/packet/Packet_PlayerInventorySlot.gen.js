32c88e40312f5360cf19b7aca8d38fec1e3cc5f2
popstarfreas <popstarfreas2@gmail.com>
2025-06-25 14:25:56 +0100

    feat: Add Packet_PlayerInventorySlot.d.ts

--- src/packet/Packet_PlayerInventorySlot.gen.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/* TypeScript file generated from Packet_PlayerInventorySlot.resi by genType. */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toBuffer = exports.parse = void 0;
-/* eslint-disable */
-/* tslint:disable */
-const Packet_PlayerInventorySlotJS = require('./Packet_PlayerInventorySlot.js');
-exports.parse = Packet_PlayerInventorySlotJS.parse;
-exports.toBuffer = Packet_PlayerInventorySlotJS.toBuffer;
