baebd6861357d800e44d07e82a2ed118d5985b3f
popstarfreas <popstarfreas2@gmail.com>
2026-01-28 09:05:37 +0000

    fix: Fix incorrect d.ts bindings

--- src/IParser.d.ts
+++ src/IParser.d.ts
@@ -8,77 +8,77 @@ import type { t as PacketType } from "./PacketType.js";
 
 export type parseError =
   | { TAG: "ReaderError"; _0: readError }
-  | { TAG: "ConnectRequestFromServer" }
-  | { TAG: "DisconnectFromClient" }
-  | { TAG: "PlayerSlotSetFromClient" }
-  | { TAG: "WorldDataRequestFromServer" }
-  | { TAG: "WorldInfoFromClient" }
-  | { TAG: "InitialTileSectionsRequestFromServer" }
-  | { TAG: "StatusFromClient" }
-  | { TAG: "TileSectionSendFromClient" }
-  | { TAG: "TileSectionFrameFromClient" }
-  | { TAG: "PlayerActiveFromClient" }
-  | { TAG: "TimeSetFromClient" }
-  | { TAG: "NpcUpdateFromClient" }
-  | { TAG: "ChestOpenFromServer" }
-  | { TAG: "PasswordRequiredFromClient" }
-  | { TAG: "PasswordSendFromServer" }
-  | { TAG: "ItemOwnerRemoveFromClient" }
-  | { TAG: "SignReadFromServer" }
-  | { TAG: "PlayerSpawnSelfFromClient" }
-  | { TAG: "NpcBuffUpdateFromClient" }
-  | { TAG: "GoodEvilUpdateFromClient" }
-  | { TAG: "BossOrInvasionSpawnFromServer" }
-  | { TAG: "ClientUuidFromServer" }
-  | { TAG: "NpcCatchFromServer" }
-  | { TAG: "NpcReleaseFromServer" }
-  | { TAG: "TravellingMerchantInventoryFromClient" }
-  | { TAG: "AnglerQuestFromClient" }
-  | { TAG: "AnglerQuestCompleteFromServer" }
-  | { TAG: "TemporaryAnimationCreateFromClient" }
-  | { TAG: "InvasionProgressReportFromClient" }
-  | { TAG: "PlayerChestIndexSyncFromClient" }
-  | { TAG: "CombatNumberCreateFromClient" }
-  | { TAG: "NpcKillCountFromClient" }
-  | { TAG: "ItemForceIntoNearestChestFromServer" }
-  | { TAG: "TileEntityUpdateFromClient" }
-  | { TAG: "TileEntityPlaceFromServer" }
-  | { TAG: "ItemDropModifyFromClient" }
-  | { TAG: "ItemFramePlaceFromServer" }
-  | { TAG: "EmoteBubbleFromClient" }
-  | { TAG: "PortalKillFromServer" }
-  | { TAG: "NpcKilledNotificationFromClient" }
-  | { TAG: "EventNotificationFromClient" }
-  | { TAG: "ShieldStrengthsUpdateFromClient" }
-  | { TAG: "MoonLordCountdownFromClient" }
-  | { TAG: "NpcShopItemFromClient" }
-  | { TAG: "GemLockToggleFromServer" }
-  | { TAG: "SmokePoofFromClient" }
-  | { TAG: "ChatMessageSmartFromClient" }
-  | { TAG: "WiredCannonShotFromClient" }
-  | { TAG: "MassWireOperationFromServer" }
-  | { TAG: "MassWireOperationPayFromClient" }
-  | { TAG: "PartyToggleFromServer" }
-  | { TAG: "CrystalInvasionStartFromServer" }
-  | { TAG: "CrystalInvasionWipeAllFromClient" }
-  | { TAG: "CrystalInvasionSendWaitTimeFromClient" }
-  | { TAG: "CombatTextCreateFromClient" }
-  | { TAG: "EmojiFromServer" }
-  | { TAG: "WeaponsRackTryPlacingFromServer" }
-  | { TAG: "RevengeMarkerSyncFromClient" }
-  | { TAG: "RevengeMarkerRemoveFromClient" }
-  | { TAG: "ClientFinishConnectingToServerFromClient" }
-  | { TAG: "NpcFishOutFromServer" }
-  | { TAG: "NpcTamperFromClient" }
-  | { TAG: "LegacySoundPlayFromClient" }
-  | { TAG: "FoodPlatterTryPlacingFromServer" }
-  | { TAG: "PlayerDeadFromClient" }
-  | { TAG: "NpcBuffRemovalRequestFromServer" }
-  | { TAG: "ClientSyncedInventoryFromServer" }
-  | { TAG: "NotImplemented" }
   | { TAG: "InvalidPacketLength"; _0: number }
   | { TAG: "InvalidPacketType"; _0: number }
-  | { TAG: "IgnoredPacket" };
+  | "ConnectRequestFromServer"
+  | "DisconnectFromClient"
+  | "PlayerSlotSetFromClient"
+  | "WorldDataRequestFromServer"
+  | "WorldInfoFromClient"
+  | "InitialTileSectionsRequestFromServer"
+  | "StatusFromClient"
+  | "TileSectionSendFromClient"
+  | "TileSectionFrameFromClient"
+  | "PlayerActiveFromClient"
+  | "TimeSetFromClient"
+  | "NpcUpdateFromClient"
+  | "ChestOpenFromServer"
+  | "PasswordRequiredFromClient"
+  | "PasswordSendFromServer"
+  | "ItemOwnerRemoveFromClient"
+  | "SignReadFromServer"
+  | "PlayerSpawnSelfFromClient"
+  | "NpcBuffUpdateFromClient"
+  | "GoodEvilUpdateFromClient"
+  | "BossOrInvasionSpawnFromServer"
+  | "ClientUuidFromServer"
+  | "NpcCatchFromServer"
+  | "NpcReleaseFromServer"
+  | "TravellingMerchantInventoryFromClient"
+  | "AnglerQuestFromClient"
+  | "AnglerQuestCompleteFromServer"
+  | "TemporaryAnimationCreateFromClient"
+  | "InvasionProgressReportFromClient"
+  | "PlayerChestIndexSyncFromClient"
+  | "CombatNumberCreateFromClient"
+  | "NpcKillCountFromClient"
+  | "ItemForceIntoNearestChestFromServer"
+  | "TileEntityUpdateFromClient"
+  | "TileEntityPlaceFromServer"
+  | "ItemDropModifyFromClient"
+  | "ItemFramePlaceFromServer"
+  | "EmoteBubbleFromClient"
+  | "PortalKillFromServer"
+  | "NpcKilledNotificationFromClient"
+  | "EventNotificationFromClient"
+  | "ShieldStrengthsUpdateFromClient"
+  | "MoonLordCountdownFromClient"
+  | "NpcShopItemFromClient"
+  | "GemLockToggleFromServer"
+  | "SmokePoofFromClient"
+  | "ChatMessageSmartFromClient"
+  | "WiredCannonShotFromClient"
+  | "MassWireOperationFromServer"
+  | "MassWireOperationPayFromClient"
+  | "PartyToggleFromServer"
+  | "CrystalInvasionStartFromServer"
+  | "CrystalInvasionWipeAllFromClient"
+  | "CrystalInvasionSendWaitTimeFromClient"
+  | "CombatTextCreateFromClient"
+  | "EmojiFromServer"
+  | "WeaponsRackTryPlacingFromServer"
+  | "RevengeMarkerSyncFromClient"
+  | "RevengeMarkerRemoveFromClient"
+  | "ClientFinishConnectingToServerFromClient"
+  | "NpcFishOutFromServer"
+  | "NpcTamperFromClient"
+  | "LegacySoundPlayFromClient"
+  | "FoodPlatterTryPlacingFromServer"
+  | "PlayerDeadFromClient"
+  | "NpcBuffRemovalRequestFromServer"
+  | "ClientSyncedInventoryFromServer"
+  | "NotImplemented"
+  | "IgnoredPacket";
 
 export type parseResult<a> =
   | { TAG: "Ok"; _0: (a) }
